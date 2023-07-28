db51bedddf842e0c19dbae4d382fc391ffe74976
biswas98 <biswasbiplab98@gmail.com>
2023-07-28 18:59:55 +0530

    learning advancements

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-document.querySelector("#head").innerHTML = 'Test';
+
+document.querySelector('#click').addEventListener('click',function(){
+    document.querySelector('#head').textContent = 'You have clicked the btn';
+    setTimeout(function(){
+        document.querySelector('#head').textContent = '';
+    },1000);
+})
