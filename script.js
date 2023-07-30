08189d3457436546b844185bf9af8230d0f9816f
biswas98 <biswasbiplab98@gmail.com>
2023-07-30 18:43:07 +0530

    displaying x,o on every positions

--- script.js
+++ script.js
@@ -1,11 +1,77 @@
 let player = 'o';
 
-document.querySelector('.b1').addEventListener('click',function(){
-    if(player == 'o')
-        document.querySelector('.b1').innerHTML = '<img src="./resources/o.png" alt="o.png">';
-    else    
-        document.querySelector('.b1').innerHTML = '<img src="./resources/x.png" alt="x.png">';
-});
+
+const x_o_Display = (player) => {
+
+    // Top
+    document.querySelector('.t1').addEventListener('click',function(){
+        if(player == 'o')
+            document.querySelector('.t1').innerHTML = '<img src="./resources/o.png" alt="o.png">';
+        else    
+            document.querySelector('.t1').innerHTML = '<img src="./resources/x.png" alt="x.png">';
+    });
+
+    document.querySelector('.t2').addEventListener('click',function(){
+        if(player == 'o')
+            document.querySelector('.t2').innerHTML = '<img src="./resources/o.png" alt="o.png">';
+        else    
+            document.querySelector('.t2').innerHTML = '<img src="./resources/x.png" alt="x.png">';
+    });
+
+    document.querySelector('.t3').addEventListener('click',function(){
+        if(player == 'o')
+            document.querySelector('.t3').innerHTML = '<img src="./resources/o.png" alt="o.png">';
+        else    
+            document.querySelector('.t3').innerHTML = '<img src="./resources/x.png" alt="x.png">';
+    });
+
+    // Middle 
+    document.querySelector('.m1').addEventListener('click',function(){
+        if(player == 'o')
+            document.querySelector('.m1').innerHTML = '<img src="./resources/o.png" alt="o.png">';
+        else    
+            document.querySelector('.m1').innerHTML = '<img src="./resources/x.png" alt="x.png">';
+    });
+
+    document.querySelector('.m2').addEventListener('click',function(){
+        if(player == 'o')
+            document.querySelector('.m2').innerHTML = '<img src="./resources/o.png" alt="o.png">';
+        else    
+            document.querySelector('.m2').innerHTML = '<img src="./resources/x.png" alt="x.png">';
+    });
+
+    document.querySelector('.m3').addEventListener('click',function(){
+        if(player == 'o')
+            document.querySelector('.m3').innerHTML = '<img src="./resources/o.png" alt="o.png">';
+        else    
+            document.querySelector('.m3').innerHTML = '<img src="./resources/x.png" alt="x.png">';
+    });
+
+    // end 
+    document.querySelector('.e1').addEventListener('click',function(){
+        if(player == 'o')
+            document.querySelector('.e1').innerHTML = '<img src="./resources/o.png" alt="o.png">';
+        else    
+            document.querySelector('.e1').innerHTML = '<img src="./resources/x.png" alt="x.png">';
+    });
+
+    document.querySelector('.e2').addEventListener('click',function(){
+        if(player == 'o')
+            document.querySelector('.e2').innerHTML = '<img src="./resources/o.png" alt="o.png">';
+        else    
+            document.querySelector('.e2').innerHTML = '<img src="./resources/x.png" alt="x.png">';
+    });
+
+    document.querySelector('.e3').addEventListener('click',function(){
+        if(player == 'o')
+            document.querySelector('.e3').innerHTML = '<img src="./resources/o.png" alt="o.png">';
+        else    
+            document.querySelector('.e3').innerHTML = '<img src="./resources/x.png" alt="x.png">';
+    });
+
+}
 
 
 
+
+x_o_Display(player);
